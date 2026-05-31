f8803705f9c1163ce72da609b44037033bc6c02a
Trixie <hi@bsamuelsson.com>
2026-05-31 14:31:50 +0800

    Rewrite of sitemap generator, improved documentation

--- SiteMapGenerator.py
+++ SiteMapGenerator.py
@@ -4,17 +4,33 @@ from datetime import datetime
 import argparse
 import time
 
+###########################################
+# Boilerplate variables
+###########################################
+
+# Parse arguments from command line
 parser = argparse.ArgumentParser()
 parser.add_argument("-f","--force",action='store_true',help="Forces an update")
 args = parser.parse_args()
 
+# Load .env values into conf dict
 conf = dotenv_values(".env")
 authHead = {'Authorization': f"Token {conf['TOKEN_ID']}:{conf['TOKEN_SECRET']}"}
 outputPageID = int(conf['SITEMAP_ID'])
-
 endpoint = conf['ENDPOINT']
+
+# Sets current time for rate limit
 lastRequest = datetime.now()
 
+# In em, indent offset and multipler for nested values
+indent = 0.5
+multiplier = 4
+
+###########################################
+# Query function
+#
+#   Standard function for querying the API, with rate limiting etc.
+###########################################
 def getBookstack(query):
     # Rate limiting to 180 requests / min
     global lastRequest
@@ -22,7 +38,7 @@ def getBookstack(query):
     if ((now-lastRequest).seconds) < (1/3):
         time.sleep(0.5)
     
-
+    # GET from endpoint
     call = requests.get(f'{endpoint}/{query}', headers=authHead)
     if call.status_code != 200:
         print(f'Request {query} failed, result code {call.status_code}')
@@ -30,8 +46,12 @@ def getBookstack(query):
         print(f'Request {query} succeeded')
         return call.json()
 
-# Standard function to get full list of things from bookstack
-# Gets list, offsets query if response is less than total pages
+###########################################
+# Full item list function
+#
+#   Standard function to get full list of things from bookstack
+#   Gets list, offsets query if response is less than total pages
+###########################################
 def getFullList(queryType,responseAtt="data"):
     query = getBookstack(queryType)
     workingData = query[responseAtt]
@@ -45,82 +65,141 @@ def getFullList(queryType,responseAtt="data"):
 
     return workingData
 
-fullPages = getFullList('pages')
+###########################################
+# Pre-run checks
+###########################################
 
-outputPageMeta = getBookstack(f'pages/{outputPageID}')
+# Sets updateNeeded to override checks if forcing an update
+updateNeeded = args.force
 
-lastRan = datetime.fromisoformat(outputPageMeta['updated_at'].strip('Z'))
-lastUpdated = datetime.fromisoformat("2000-01-01T00:00:00")
+# Checks if force argument hasn't been passed before querying for last page update
+if not updateNeeded:
+    # Gets full list of items on instance
+    fullShelves = getFullList('shelves')
+    fullBooks = getFullList('books')
+    fullChapters = getFullList('chapters')
+    fullPages = getFullList('pages')
+    fullItems = fullShelves + fullBooks + fullChapters + fullPages
 
-if not args.force:
-    for page in fullPages:
-        if page['id'] != outputPageID:
-            updated = datetime.fromisoformat(page['updated_at'].strip('Z'))
+    # Gets the datetime of the last update to the output page (i.e: when was the Site Map last updated)
+    # NOTE: Due to datetime being datetime, have needed to strip the "Z" when using fromisoformat
+    outputPageMeta = getBookstack(f'pages/{outputPageID}')
+    lastRan = datetime.fromisoformat(outputPageMeta['updated_at'].strip('Z'))
+    lastUpdated = datetime.fromisoformat("2000-01-01T00:00:00")
+
+    # Loops through all items except output page, compares update time with the current lastUpdated.
+    # If later, overrides the lastUpdated variable
+    for item in fullItems:
+        if item['id'] != outputPageID:
+            updated = datetime.fromisoformat(item['updated_at'].strip('Z'))
             if updated > lastUpdated:
                 lastUpdated = updated
+    
+    # Compares lastUpdated (i.e: the last time anything was updated) with lastRan (i.e: when the Site Map was last updated)
+    # If there was an update since the sitemap was last changed, sets updateNeeded to True
+    if lastUpdated > lastRan:
+        updateNeeded = True
 
-if lastUpdated > lastRan or args.force:
+
+# If no update needed, no action required
+if not updateNeeded:
+    print("No update needed...")
+
+###########################################
+# Update script
+###########################################
+
+# If update needed, begins script
+else:
     print("Updating...")
 
-    shelfList = []
-
+    # Gets list of shelves and sorts by the slug
     fullShelves = getFullList('shelves')
+    shelfList = []
     for shelf in fullShelves:
-        shelfList.append((shelf['name'],shelf['id'],shelf['slug'],shelf['description']))
-        
+        shelfDict = {}
+        shelfDict['id'] = shelf['id']
+        shelfDict['slug'] = shelf['slug']
+
+        # As we cannot sort a list by a dict, generates a tuple of the slug and the dict
+        shelfList.append((shelf['slug'],shelfDict))
+
+    # NOTE: We only need to sort shelves as every other query returns sorted
     shelfList.sort()
-    shelfToBooks = {}
-    booksToContents = {}
 
-    for shelf in shelfList:
-        id = shelf[1]
-        bookList = []
-        books = getBookstack(f'shelves/{id}')['books']
-        for book in books:
-            bookList.append((book['name'],book['id'],book['slug']))
-            
-            bookID = book['id']
-            contentList = []
-            bookContents = getBookstack(f'books/{bookID}')['contents']
-            for item in bookContents:
-                if item['type'] == 'chapter':
-                    contentList.append((item['name'],'chapter',item['url']))
-                    for page in item['pages']:
-                        contentList.append((page['name'], 'page', page['url']))
-                else:
-                    contentList.append((item['name'], 'page', item['url']))
-            contentList.sort()
-            booksToContents[bookID] = contentList
-
-        bookList.sort()
-        shelfToBooks[id] = bookList
-
+    # Creates an empty list to append formatted HTML to
     htmlOutput = []
-    for shelf in shelfList:
-        head = f'<h3 class="shelf"><a href="/shelves/{shelf[2]}">{shelf[0]}</a></h3>'
-        htmlOutput.append(head)
-        desc = f'<p><i>{shelf[3]}</i></p>'
-        htmlOutput.append(desc)
 
-        for books in shelfToBooks[shelf[1]]:
-            htmlOutput.append(f'<h5 class="book"><a href="/books/{books[2]}">{books[0]}</a></h5>')
-            htmlOutput.append("<ul>")
-            for contents in booksToContents[books[1]]:
-                if contents[1] == 'chapter':
-                    htmlOutput.append(f'<li class="chapter"><b><u><a href={contents[2]}>{contents[0]}</b></u></li>')
-                else:
-                    htmlOutput.append(f'<li class="page"><a href={contents[2]}>{contents[0]}</li>')
-            htmlOutput.append("</ul>")
+    ###########################################
+    # List generation
+    #
+    #   These nested "for" loops are the bulk of actions. 
+    #   This is where each shelf, book, and chapter is looped through to and a list generated.
+    ###########################################
 
-    output = "\n".join(htmlOutput)
+    for entry in shelfList:
+        
+        # Extracts the shelfDict from the tuple we generated earlier.
+        s = entry[1]
+        
+        # Queries the specific shelf (as we will need more information than we got from just calling 'shelves')
+        shelf = getBookstack(f'shelves/{s['id']}')
 
-    postOutput = {"html": output}
+        # Formats the shelf header as a h3 with the description_html below
+        htmlOutput.append(f'<h3 class="shelf"><a href="/shelves/{shelf['slug']}">{shelf['name']}</a></h3>\n<i><u><b>{shelf['description_html']}</u></b></i>')
 
+        # Loops through each book within the shelf
+        for b in shelf['books']:
+            # Queries the specific book
+            book = getBookstack(f'books/{b['id']}')
+            
+            # Formats the shelf header as a h5
+            htmlOutput.append(f'<h5 class="book" style="margin-bottom:0.3em"><a href="/books/{book['slug']}">{book['name']}</a></h5>')
+
+            # If a description for the book has been set, adds it below the header with appropriate formatting
+            if book['description_html'] != "<p></p>":
+                desc = f'<p style="margin-top:0.3em;margin-bottom:0.3em"><u>{book['description_html'][3:-4]}</u></p>'
+                htmlOutput.append(desc)
+                
+            # If there are more than 50 pages within the book, omits the content
+            if len(book['contents']) > 50:
+                htmlOutput.append(f'<p style="padding-left: 32px"><a href="/books/{book['slug']}"><i>Contents omitted, {len(book['contents'])} pages available.</i></a></p>')
+            
+            # If there are less than 50 pages, generates the content list
+            else:
+                # Opens the unordered lsit
+                htmlOutput.append("<ul>")
+
+                # Loops through book contents
+                for item in book['contents']:
+
+                    # If the item is a chapter, bolds the item and offsets the chapter pages as 'nested' items
+                    if item['type'] == 'chapter':
+                        htmlOutput.append(f'<li style="margin-left: {str(indent)}em"><b><a href="{item['url']}">{item['name']}</a></b></li>')
+                        
+                        for page in item['pages']:
+                            htmlOutput.append(f'<li style="margin-left: {str(indent*multiplier)}em"><a href="{page['url']}">{page['name']}</a></li>')
+                    
+                    # Else, generates the list item
+                    else:
+                        htmlOutput.append(f'<li style="margin-left: {str(indent)}em"><a href="{item['url']}">{item['name']}</a></li>')
+                
+                # Closes the unordered list
+                htmlOutput.append("</ul>")
+
+        # Adds horizontal line between each shelf
+        htmlOutput.append('<hr />')
+    
+    ###########################################
+    # Final output
+    ###########################################
+    
+    # Joins the htmlOutput list as a string and generates the data for the PUT request
+    postOutput = {"html": "\n".join(htmlOutput)}
+
+    # PUTs the output over the outputPage's contents
     call = requests.put(f'{endpoint}/pages/{outputPageID}', headers=authHead, data=postOutput)
     if call.status_code != 200:
         print(f'Final PUT failed, result code {call.status_code}')
     else:
         print(f'Final PUT succeeded')
-
-else:
-    print("No update needed")
